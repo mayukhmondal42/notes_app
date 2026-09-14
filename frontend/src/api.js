fa1c18d3398e10c205c5b5a4cdc8692260fd539e
mayukhmondal42 <mayukhmondal42@gmail.com>
2026-09-15 01:47:30 +0530

    fast api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -4,7 +4,6 @@ const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5001";
 
 const api = axios.create({
   baseURL: `${API_URL}/api`,
-  timeout: 8000,
 });
 
 api.interceptors.request.use((config) => {
